26c8ec1ee5c543ef3484b244a9ca304d188dd173
dellpatcher <a1sxgamz@gmail.com>
2025-08-26 15:38:34 -0700

    update

--- Void_scrubber/bpf/drop_spoof.c
+++ Void_scrubber/bpf/drop_spoof.c
@@ -44,19 +44,49 @@ int xdp_drop_scrub(struct xdp_md *ctx) {
     if ((void*)ip + (ip->ihl * 4) > data_end)
         return XDP_DROP;
 
+    // Drop short IP packets (less than header size)
+    if (__bpf_ntohs(ip->tot_len) < (ip->ihl * 4))
+        return XDP_DROP;
+
     // L4: TCP/UDP/ICMP DDoS basic stateless filtering
     if (ip->protocol == IPPROTO_TCP) {
         struct tcphdr *tcp = (void*)ip + (ip->ihl * 4);
         if ((void*)(tcp + 1) > data_end)
             return XDP_DROP;
 
-        // Drop XMAS, NULL, and other malformed TCP packets
-        if ((tcp->fin && tcp->urg && tcp->psh) || 
-            (tcp->syn == 0 && tcp->ack == 0 && tcp->fin == 0 && tcp->rst == 0 && tcp->psh == 0 && tcp->urg == 0))
+        // Drop XMAS scan: FIN, PSH, URG all set
+        if (tcp->fin && tcp->psh && tcp->urg)
+            return XDP_DROP;
+
+        // Drop NULL scan: no flags set
+        if (!(tcp->fin || tcp->syn || tcp->rst || tcp->psh || tcp->ack || tcp->urg))
+            return XDP_DROP;
+
+        // Drop FIN scan: only FIN set
+        if (tcp->fin && !(tcp->syn || tcp->rst || tcp->psh || tcp->ack || tcp->urg))
             return XDP_DROP;
 
         // Drop SYN floods (stateless: drop if only SYN, no ACK, no payload)
         if (tcp->syn && !tcp->ack && ((void*)(tcp + 1) == data_end))
+            return XDP_DROP;
+
+        // Drop ACK floods (only ACK, no payload)
+        if (tcp->ack && !(tcp->syn || tcp->fin || tcp->rst || tcp->psh || tcp->urg) && ((void*)(tcp + 1) == data_end))
+            return XDP_DROP;
+
+        // Drop RST floods (only RST, no payload)
+        if (tcp->rst && !(tcp->syn || tcp->fin || tcp->ack || tcp->psh || tcp->urg) && ((void*)(tcp + 1) == data_end))
+            return XDP_DROP;
+
+        // Drop PSH+ACK floods (only PSH+ACK, no payload)
+        if (tcp->psh && tcp->ack && !(tcp->syn || tcp->fin || tcp->rst || tcp->urg) && ((void*)(tcp + 1) == data_end))
+            return XDP_DROP;
+
+        // Block Mirai/QBot common ports (telnet, web, TR-069, etc.)
+        __u16 dport = __bpf_ntohs(tcp->dest);
+        if (dport == 23   || dport == 2323  || // Telnet
+            dport == 80   || dport == 81    || dport == 82   || dport == 8080 || // Web
+            dport == 7547 || dport == 5555  || dport == 37215) // TR-069, Mirai, QBot
             return XDP_DROP;
     } else if (ip->protocol == IPPROTO_UDP) {
         struct udphdr *udp = (void*)ip + (ip->ihl * 4);
@@ -67,9 +97,15 @@ int xdp_drop_scrub(struct xdp_md *ctx) {
         if (__bpf_ntohs(udp->len) < 8)
             return XDP_DROP;
 
-        // Drop UDP floods to common amplification ports (NTP, DNS, SSDP, Memcached)
+        // Drop UDP floods to common amplification ports (NTP, DNS, SSDP, Memcached, Chargen, TFTP)
         __u16 dport = __bpf_ntohs(udp->dest);
-        if (dport == 53 || dport == 123 || dport == 1900 || dport == 11211)
+        if (dport == 53 || dport == 123 || dport == 1900 || dport == 11211 || dport == 19 || dport == 69)
+            return XDP_DROP;
+
+        // Block Mirai/QBot common ports (telnet, web, TR-069, etc.)
+        if (dport == 23   || dport == 2323  ||
+            dport == 80   || dport == 81    || dport == 82   || dport == 8080 ||
+            dport == 7547 || dport == 5555  || dport == 37215)
             return XDP_DROP;
     } else if (ip->protocol == IPPROTO_ICMP) {
         struct icmphdr *icmp = (void*)ip + (ip->ihl * 4);
@@ -79,6 +115,11 @@ int xdp_drop_scrub(struct xdp_md *ctx) {
         // Drop ICMP echo-request floods (type 8)
         if (icmp->type == 8)
             return XDP_DROP;
+    } else if (ip->protocol == 47) { // GRE (Generic Routing Encapsulation)
+        // Mirai/QBot sometimes use GRE floods
+        return XDP_DROP;
+    } else if (ip->protocol == 41) { // IPv6-in-IPv4 tunneling
+        return XDP_DROP;
     }
 
     return XDP_PASS;
--- Void_scrubber/config.py
+++ Void_scrubber/config.py
@@ -19,31 +19,52 @@ table inet void_scrubber {
         allowed_dns {
             type ipv4_addr
         }
+        allowed_ntp {
+            type ipv4_addr
+        }
     }
 
     chains {
         input {
             type filter hook input priority 0; policy drop;
 
+            # Accept established/related
             ct state established,related accept
-            ip saddr @trusted_ips accept
+
+            # Anti-spoofing: drop packets from reserved/bogon sources
+            ip saddr { 0.0.0.0/8, 10.0.0.0/8, 100.64.0.0/10, 127.0.0.0/8, 169.254.0.0/16, 172.16.0.0/12, 192.0.2.0/24, 192.168.0.0/16, 198.18.0.0/15, 198.51.100.0/24, 203.0.113.0/24, 224.0.0.0/4, 240.0.0.0/4 } drop
+            ip6 saddr { ::1/128, fc00::/7, fe80::/10, ff00::/8 } drop
+
+            # Drop blacklisted IPs
             ip saddr @blacklist drop
+
+            # Allow trusted IPs
+            ip saddr @trusted_ips accept
 
             # Allow DNS from trusted servers only
             udp dport 53 ip saddr @allowed_dns accept
 
+            # Allow NTP from trusted servers only
+            udp dport 123 ip saddr @allowed_ntp accept
+
             # Allow DHCP
             udp sport 67 udp dport 68 accept
+            udp sport 547 udp dport 546 accept
 
             # Allow ICMP echo-request (ping) from trusted only
             ip protocol icmp icmp type echo-request ip saddr @trusted_ips accept
+            ip6 nexthdr icmpv6 icmpv6 type { 128 } ip6 saddr @trusted_ips accept
 
-            # Limit SSH, HTTP, HTTPS
-            tcp dport {22, 80, 443} ct state new limit rate 50/second burst 100 packets accept
+            # Allow essential ICMPv6 (ND, RA, etc.)
+            ip6 nexthdr icmpv6 icmpv6 type { 133, 134, 135, 136, 137 } accept
+
+            # Limit SSH, HTTP, HTTPS (new connections)
+            tcp dport {22, 80, 443} ct state new limit rate 30/second burst 60 packets accept
 
             # Drop invalid packets
             ct state invalid drop
 
+            # Log and drop everything else
             log prefix "VOID_DROP: " flags all counter
             drop
         }
